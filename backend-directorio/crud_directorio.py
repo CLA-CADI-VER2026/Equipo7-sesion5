39a2f4a3b45e0999c96c27a49c929d2a0d9251e1
Fernando Agustín Ramírez Mayorga <framirezm@tec.mx>
2026-06-26 16:21:09 +0000

    sinc

--- backend-directorio/crud_directorio.py
+++ backend-directorio/crud_directorio.py
@@ -5,7 +5,7 @@ conn = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
     password="contrasena",
-    database="curso_db",
+    database="directorio",
     port=3306
 )
 cursor = conn.cursor(dictionary=True)
